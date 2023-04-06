c387dba770f18e252efe364526dd8f963cc461d2
Tobias Zwick <newton@westnordost.de>
2023-04-07 01:54:34 +0200

    fail early when parsing obviously wrong data

--- library/src/main/java/de/westnordost/countryboundaries/CountryBoundariesDeserializer.java
+++ library/src/main/java/de/westnordost/countryboundaries/CountryBoundariesDeserializer.java
@@ -8,23 +8,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// TODO proper exceptions...
 class CountryBoundariesDeserializer {
     public CountryBoundaries read(DataInputStream in) throws IOException {
         int version = in.readUnsignedShort();
         if (version == 44269) { // old/unversioned boundaries file format
-            throw new IOException("The boundaries file serialization format changed. If you are using the default data, get the updated data from https://github.com/westnordost/countryboundaries/tree/master/data , otherwise, you need to re-generate the data with the current version of the generator in the same repository.");
+            throw new IOException("The file serialization format changed. If you are using the default data, get the updated data from https://github.com/westnordost/countryboundaries/tree/master/data , otherwise, you need to re-generate the data with the current version of the generator found in the same repository.");
         } else if (version != 2) {
-            throw new IOException("Wrong version number '" + version + "' of the boundaries file (expected: '2'). You may need to get the current version of the data.");
+            throw new IOException("Wrong version number '" + version + "' of the file serialization format (expected: '2'). You may need to get the current version of the data.");
         }
 
         int geometrySizesCount = in.readInt();
+        if (geometrySizesCount < 0) throw new IOException("Invalid data");
         Map<String, Double> geometrySizes = new HashMap<>(geometrySizesCount);
         for (int i = 0; i < geometrySizesCount; i++) {
             geometrySizes.put(in.readUTF().intern(), in.readDouble());
         }
         int rasterWidth = in.readInt();
+        if (rasterWidth < 0) throw new IOException("Invalid data");
         int rasterSize = in.readInt();
+        if (rasterSize < 0) throw new IOException("Invalid data");
         CountryBoundariesCell[] raster = new CountryBoundariesCell[rasterSize];
         for (int i= 0; i < rasterSize; i++) {
             raster[i] = readCell(in);
@@ -69,7 +71,9 @@ class CountryBoundariesDeserializer {
     }
 
     private Point[] readRing(DataInputStream in) throws IOException {
-        Point[] ring = new Point[in.readInt()];
+        int ringSize = in.readInt();
+        if (ringSize < 0) throw new IOException("Invalid data");
+        Point[] ring = new Point[ringSize];
         for (int j = 0; j < ring.length; j++) {
             ring[j] = readPoint(in);
         }
